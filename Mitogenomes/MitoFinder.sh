d7ccbcc93929ee3bc61d03db8b6c8651bf4cd59b
cbreusing <77281303+cbreusing@users.noreply.github.com>
2024-08-07 15:28:52 -0400

    Update MitoFinder.sh

--- Mitogenomes/MitoFinder.sh
+++ Mitogenomes/MitoFinder.sh
@@ -18,7 +18,7 @@ TABLE=$(sed -n "${SGE_TASK_ID}p" assembly.list | perl -anle 'print $F[2]')
 # Option 1: Assemble and annotate from trimmed reads
 #mitofinder -j ${FILE} -1 ../${FILE}_R1_clean.fastq -2 ../${FILE}_R2_clean.fastq -r /scratch/nmnh_mdbc/breusingc/databases/${TAXA}_reference.gb --max-contig-size 80000 --ignore -o ${TABLE} -p ${NSLOTS} -m 32 -t mitfi --metaspades --rename-contig yes --new-genes --allow-intron
 
-# Option 2: Annotate from previously assembled metagenome (in case first option fails or for re-annotation of curaed mitogenome)
+# Option 2: Annotate from previously assembled metagenome (in case first option fails or for re-annotation of curated mitogenome)
 mitofinder -j ${FILE} -a ../metagenomes/${FILE}_metaspades/scaffolds.fasta -r /scratch/nmnh_mdbc/breusingc/databases/${TAXA}_reference.gb --max-contig-size 80000 --ignore -o ${TABLE} -p ${NSLOTS} -m 32 -t mitfi --rename-contig yes --new-genes --allow-intron
 
 echo = `date` job $JOB_NAME $SGE_TASK_ID done
