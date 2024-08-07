fd792a43671e9e900df59052ff4f92e1a8884fcb
cbreusing <77281303+cbreusing@users.noreply.github.com>
2024-08-07 12:38:45 -0400

    Update AssembleBarcodes.sh

--- Barcodes/AssembleBarcodes.sh
+++ Barcodes/AssembleBarcodes.sh
@@ -3,6 +3,7 @@
 #$ -q sThC.q
 #$ -pe mthread 24
 #$ -l mres=96G,h_data=4G,h_vmem=4G
+#$ -l ssd_res=200G -v SSD_SAVE_MAX=0
 #$ -cwd
 #$ -j y
 #$ -N AssembleBarcodes
@@ -12,6 +13,7 @@
 module load bio/bbmap
 module load bio/spades
 module load bio/blast
+module load tools/ssd
 
 # Run this only once to prepare reference databases for BBMap
 #bbmap.sh ref=/scratch/dbs/blast/bold/fasta/bold_coi_2023_05_12.fasta build=1 
@@ -20,9 +22,12 @@ module load bio/blast
 
 FILE=$(sed -n "${SGE_TASK_ID}p" filelist.txt | perl -anle 'print $F[0]')
 
-#Extract COI reads
-bbmap.sh build=1 t=${NSLOTS} in=${FILE}_R1_clean.fastq in2=${FILE}_R2_clean.fastq ambiguous=best pairedonly=t outm=${FILE}_COI_#.fastq
-metaspades.py -1 ${FILE}_COI_1.fastq -2 ${FILE}_COI_2.fastq -t ${NSLOTS} -o spades_${FILE}_COI
+Extract COI reads
+bbmap.sh build=1 t=${NSLOTS} in=../${FILE}_R1_clean.fastq in2=../${FILE}_R2_clean.fastq ambiguous=best pairedonly=t outm=${FILE}_COI_#.fastq
+repair.sh in1=${FILE}_COI_1.fastq in2=${FILE}_COI_2.fastq out1=${FILE}_COIfixed_1.fastq out2=${FILE}_COIfixed_2.fastq repair
+mv ${FILE}_COIfixed_1.fastq ${FILE}_COI_1.fastq
+mv ${FILE}_COIfixed_2.fastq ${FILE}_COI_2.fastq
+metaspades.py -1 ${FILE}_COI_1.fastq -2 ${FILE}_COI_2.fastq -t ${NSLOTS} -o spades_${FILE}_COI --tmp-dir $SSD_DIR/${FILE}_COI
 blastn -query spades_${FILE}_COI/scaffolds.fasta -db /scratch/dbs/blast/bold/bold_blast_2023_05_12 -out blast.${FILE}.COI.txt -evalue 1e-10 -num_threads ${NSLOTS} -max_target_seqs 1 -outfmt 6
 cat blast.${FILE}.COI.txt | sort -k1,1 -k12,12nr -k11,11n | sort -u -k1,1 > blast.${FILE}.COI.topHit.txt
 perl /home/breusingc/scripts/reformat_table.pl blast.${FILE}.COI.topHit.txt /scratch/nmnh_mdbc/breusingc/databases/bold_coi_2023_05_12.txt blast.${FILE}.COI.topHit.full.txt
@@ -34,16 +39,24 @@ perl /home/breusingc/scripts/reformat_table2.pl blast.${FILE}.mito.topHit.txt /s
 rm blast.${FILE}.mito.txt
 rm blast.${FILE}.mito.topHit.txt
 
-#Extract 18S reads
-bbmap.sh build=2 t=${NSLOTS} in=${FILE}_R1_clean.fastq in2=${FILE}_R2_clean.fastq ambiguous=best pairedonly=t outm=${FILE}_18S_#.fastq
-metaspades.py -1 ${FILE}_18S_1.fastq -2 ${FILE}_18S_2.fastq -t ${NSLOTS} -o spades_${FILE}_18S
-blastn -query spades_${FILE}_18S/scaffolds.fasta -db /scratch/nmnh_mdbc/breusingc/databases/SILVA_138.1_SSURef_NR99 -out blast.${FILE}.18S.txt -evalue 1e-10 -num_threads ${NSLOTS} -max_target_seqs 1 -outfmt "6 std stitle"
+Extract 18S reads
+bbmap.sh build=2 t=${NSLOTS} in=../${FILE}_R1_clean.fastq in2=../${FILE}_R2_clean.fastq ambiguous=best pairedonly=t outm=${FILE}_18S_#.fastq
+repair.sh in1=${FILE}_18S_1.fastq in2=${FILE}_18S_2.fastq out1=${FILE}_18Sfixed_1.fastq out2=${FILE}_18Sfixed_2.fastq repair
+mv ${FILE}_18Sfixed_1.fastq ${FILE}_18S_1.fastq
+mv ${FILE}_18Sfixed_2.fastq ${FILE}_18S_2.fastq
+metaspades.py -1 ${FILE}_18S_1.fastq -2 ${FILE}_18S_2.fastq -t ${NSLOTS} -o spades_${FILE}_18S --tmp-dir $SSD_DIR/${FILE}_18S
+blastn -query spades_${FILE}_18S/scaffolds.fasta -db /scratch/nmnh_mdbc/breusingc/databases/SILVA_138.1_SSURef_NR99 -out blast.${FILE}.18S.txt -evalue 1e-10 -num_threads ${NSLOTS} -max_target_seqs 1 -out
+fmt "6 std stitle"
 cat blast.${FILE}.18S.txt | sort -k1,1 -k12,12nr -k11,11n | sort -u -k1,1 > blast.${FILE}.18S.topHit.txt
 rm blast.${FILE}.18S.txt
 
-#Extract 28S reads
-bbmap.sh build=3 t=${NSLOTS} in=${FILE}_R1_clean.fastq in2=${FILE}_R2_clean.fastq ambiguous=best pairedonly=t outm=${FILE}_28S_#.fastq
-metaspades.py -1 ${FILE}_28S_1.fastq -2 ${FILE}_28S_2.fastq -t ${NSLOTS} -o spades_${FILE}_28S
-blastn -query spades_${FILE}_28S/scaffolds.fasta -db /scratch/nmnh_mdbc/breusingc/databases/SILVA_138.1_LSURef_NR99 -out blast.${FILE}.28S.txt -evalue 1e-10 -num_threads ${NSLOTS} -max_target_seqs 1 -outfmt "6 std stitle"
+Extract 28S reads
+bbmap.sh build=3 t=${NSLOTS} in=../${FILE}_R1_clean.fastq in2=../${FILE}_R2_clean.fastq ambiguous=best pairedonly=t outm=${FILE}_28S_#.fastq
+repair.sh in1=${FILE}_28S_1.fastq in2=${FILE}_28S_2.fastq out1=${FILE}_28Sfixed_1.fastq out2=${FILE}_28Sfixed_2.fastq repair
+mv ${FILE}_28Sfixed_1.fastq ${FILE}_28S_1.fastq
+mv ${FILE}_28Sfixed_2.fastq ${FILE}_28S_2.fastq
+metaspades.py -1 ${FILE}_28S_1.fastq -2 ${FILE}_28S_2.fastq -t ${NSLOTS} -o spades_${FILE}_28S --tmp-dir $SSD_DIR/${FILE}_28S
+blastn -query spades_${FILE}_28S/scaffolds.fasta -db /scratch/nmnh_mdbc/breusingc/databases/SILVA_138.1_LSURef_NR99 -out blast.${FILE}.28S.txt -evalue 1e-10 -num_threads ${NSLOTS} -max_target_seqs 1 -ou
+tfmt "6 std stitle"
 cat blast.${FILE}.28S.txt | sort -k1,1 -k12,12nr -k11,11n | sort -u -k1,1 > blast.${FILE}.28S.topHit.txt
 rm blast.${FILE}.28S.txt
